67bd45b9c845f4f1a79d233f0a0c6b872aebdb37
Edelweiss2021 <95542755+Edelweiss2021@users.noreply.github.com>
2022-03-29 13:38:13 +0300

    Update prime_numbers_generator.py
    Additional solution added

--- prime_numbers_generator.py
+++ prime_numbers_generator.py
@@ -15,3 +15,28 @@ for i in range(2, num+1):
   if isPrime:
     prime_numbers.append(i)
 print(prime_numbers)
+
+# # /////////////////////
+
+# # Python program to print first 10 prime numbers
+
+# # A function name prime is defined using def
+
+# def prime(n):
+# 	x = 1
+# 	count = 0
+# 	while count < n:
+# 		for d in range(2, x, 1):
+# 			if x % d == 0:
+# 				x += 1
+# 		else:
+# 			print(x)
+# 			x += 1
+# 			count += 1
+
+# # Driver Code
+# n = 10
+
+# # print statement
+# print("First 10 prime numbers are: ")
+# prime(n)
